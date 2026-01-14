f33d5a0a5d22a4535895cc6e2c92ac6bad614d09
jryurgil <jryurgil@gmail.com>
2026-01-14 18:24:16 -0500

    inital kitbot code

---
+++ src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.DriveConstants.*;
+
+public class CANDriveSubsystem extends SubsystemBase {
+  
+  /* private final SparkMax leftLeader;
+  private final SparkMax leftFollower;
+  private final SparkMax rightLeader;
+  private final SparkMax rightFollower; */
+
+  private final TalonSRX leftLeader;
+  private final TalonSRX leftFollower;
+  private final TalonSRX rightLeader;
+  private final TalonSRX rightFollower;
+
+  public CANDriveSubsystem() {
+    // create brushed motors for drive
+    /* leftLeader = new SparkMax(LEFT_LEADER_ID, MotorType.kBrushed);
+    leftFollower = new SparkMax(LEFT_FOLLOWER_ID, MotorType.kBrushed);
+    rightLeader = new SparkMax(RIGHT_LEADER_ID, MotorType.kBrushed);
+    rightFollower = new SparkMax(RIGHT_FOLLOWER_ID, MotorType.kBrushed); */
+
+    leftLeader = new TalonSRX(LEFT_LEADER_ID);
+    leftFollower = new TalonSRX(LEFT_FOLLOWER_ID);
+    rightFollower = new TalonSRX(RIGHT_FOLLOWER_ID);
+    rightLeader = new TalonSRX(RIGHT_LEADER_ID);
+
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  public void driveArcade(double xSpeed, double zRotation) {
+    //create drive code
+    leftLeader.set(TalonSRXControlMode.PercentOutput,xSpeed+zRotation);
+    leftFollower.set(TalonSRXControlMode.PercentOutput,xSpeed+zRotation);
+    rightLeader.set(TalonSRXControlMode.PercentOutput,-xSpeed+zRotation);
+    rightFollower.set(TalonSRXControlMode.PercentOutput,-xSpeed+zRotation);
+    
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.FuelConstants.*;
+
+public class CANFuelSubsystem extends SubsystemBase {
+  /* private final SparkMax feederRoller;
+  private final SparkMax intakeLauncherRoller; */
+private final TalonSRX feederRoller;
+private final TalonSRX intakeLauncherRoller;
+
+
+  /** Creates a new CANBallSubsystem. */
+  public CANFuelSubsystem() {
+    // create brushed motors for each of the motors on the launcher mechanism
+    /* intakeLauncherRoller = new SparkMax(INTAKE_LAUNCHER_MOTOR_ID, MotorType.kBrushed);
+    feederRoller = new SparkMax(FEEDER_MOTOR_ID, MotorType.kBrushed); */
+
+  intakeLauncherRoller = new TalonSRX(INTAKE_LAUNCHER_MOTOR_ID);
+  feederRoller = new TalonSRX(FEEDER_MOTOR_ID);
+
+    
+    // put default values for various fuel operations onto the dashboard
+    // all commands using this subsystem pull values from the dashbaord to allow
+    // you to tune the values easily, and then replace the values in Constants.java
+    // with your new values. For more information, see the Software Guide.
+    SmartDashboard.putNumber("Intaking feeder roller value", INTAKING_FEEDER_VOLTAGE);
+    SmartDashboard.putNumber("Intaking intake roller value", INTAKING_INTAKE_VOLTAGE);
+    SmartDashboard.putNumber("Launching feeder roller value", LAUNCHING_FEEDER_VOLTAGE);
+    SmartDashboard.putNumber("Launching launcher roller value", LAUNCHING_LAUNCHER_VOLTAGE);
+    SmartDashboard.putNumber("Spin-up feeder roller value", SPIN_UP_FEEDER_VOLTAGE);
+  }
+
+  // A method to set the voltage of the intake roller
+  public void setIntakeLauncherRoller(double voltage) {
+    //intakeLauncherRoller.setVoltage(voltage);
+    intakeLauncherRoller.set(TalonSRXControlMode.PercentOutput, voltage);
+  }
+
+  // A method to set the voltage of the intake roller
+  public void setFeederRoller(double voltage) {
+    //feederRoller.setVoltage(voltage);
+    feederRoller.set(TalonSRXControlMode.PercentOutput, voltage);
+  }
+
+  // A method to stop the rollers
+  public void stop() {
+    feederRoller.set(TalonSRXControlMode.PercentOutput,0);
+    intakeLauncherRoller.set(TalonSRXControlMode.PercentOutput,0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
